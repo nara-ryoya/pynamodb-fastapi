f34e4890e083396824e3e6edc85e41b46d11e4ad
nara-ryoya <nara_ryoya@icloud.com>
2022-09-15 01:48:40 +0900

    feat: データ型を定義

---
+++ backend/api/models/Tsundoku.py
@@ -0,0 +1,37 @@
+from pynamodb.attributes import (
+    MapAttribute,
+    NumberAttribute,
+    UnicodeAttribute,
+    UTCDateTimeAttribute,
+)
+from pynamodb.indexes import AllProjection, GlobalSecondaryIndex
+from pynamodb.models import Model
+
+
+class Book(MapAttribute):
+    counter = NumberAttribute()
+    author = UnicodeAttribute()
+    category = UnicodeAttribute()
+    thoughts = UnicodeAttribute()
+
+
+class DoneIndex(GlobalSecondaryIndex):
+    class Meta:
+        index_name = "list_by_done"
+        read_capacity_units = 2
+        write_capacity_units = 1
+        projection = AllProjection()
+
+    done = UnicodeAttribute(hash_key=True)
+
+
+class Tsundoku(Model):
+    class Meta:
+        host: str
+        table_name: str
+        region: str
+
+    user_id = UnicodeAttribute(hash_key=True)
+    timestamp = UTCDateTimeAttribute(range_key=True)
+    done = UnicodeAttribute()
+    done_index = DoneIndex()
